c60acd5e74bce0e271bf2deb8ffefc75017cc455
Elena Mamontova <helenh463@gmail.com>
2020-01-10 16:31:43 +0200

    The long-name-file argument fixed to log-file-name, lines with rguments -a, -d, -o commented out in the reference function.

--- server.cpp
+++ server.cpp
@@ -4,11 +4,12 @@
 void reference()
 {
     std::cout << "Usage:\n";
-    std::cout << "server [-a/--address <bind-address>][-d/--dir <work-dir>][-o/--out-log <long-file-name>][-h/--help][-v/--version]\n\n";
+    std::cout << "server [-h/--help][-v/--version]\n\n";
+/*    std::cout << "server [-a/--address <bind-address>][-d/--dir <work-dir>][-o/--out-log <log-file-name>][-h/--help][-v/--version]\n\n";
 
     std::cout << "-a, --address <bind-address> - can be specified in <address>[:<port>] form, where <address> can be IP-address or domain name, <port> - number;\n";
     std::cout << "-d, --dir <work-dir> - specifies the working directory with files for the server;\n";
-    std::cout << "-o, --out-log <long-file-name> - specifies the file name for logging for the server;\n";
+    std::cout << "-o, --out-log <log-file-name> - specifies the file name for logging for the server;\n";*/
     std::cout << "-v, --version - server version;\n";
     std::cout << "-h, --help - show this text.\n";
 }
