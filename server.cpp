e52ca6a1d08673cc030ec8c0dfde0228d3e567f2
Elena Mamontova <helenh463@gmail.com>
2020-01-28 15:53:01 +0200

    Header files string, ctime, boost/asio.hpp added. Function make_daytime_string() added. Option -a added when parsing the command line.

--- server.cpp
+++ server.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
 
 void reference()
 {
     std::cout << "Usage:\n";
-    std::cout << "server [-h/--help][-v/--version]\n\n";
+    std::cout << "server [-a/--address <bind-address][-h/--help][-v/--version]\n\n";
 /*    std::cout << "server [-a/--address <bind-address>][-d/--dir <work-dir>][-o/--out-log <log-file-name>][-h/--help][-v/--version]\n\n";
 
-    std::cout << "-a, --address <bind-address> - can be specified in <address>[:<port>] form, where <address> can be IP-address or domain name, <port> - number;\n";
     std::cout << "-d, --dir <work-dir> - specifies the working directory with files for the server;\n";
     std::cout << "-o, --out-log <log-file-name> - specifies the file name for logging for the server;\n";*/
+    std::cout << "-a, --address <bind-address> - can be specified in <address>[:<port>] form, where <address> can be IP-address or domain name, <port> - number;\n";
     std::cout << "-v, --version - server version;\n";
     std::cout << "-h, --help - show this text.\n";
+}
+
+std::string make_daytime_string()
+{
+  using namespace std; // For time_t, time and ctime;
+  time_t now = time(0);
+  return ctime(&now);
 }
 
 int main(int argc, char* argv[])
 {
     const std::string version = "1.0.0";
+    std::string address;
 
     for (int i = 1; i < argc; ++i)
     {
         const std::string arg = argv[i];
-        if (arg == "-v" || arg == "--version")
+
+        if (arg == "-a" || arg == "--address")
+        {
+            if (i + 1 == argc)
+            {
+                std::cerr << argv[i] << " needs and argument - an address.\n";
+                return 1;
+            }
+            address = argv[++i];
+        }
+        else if (arg == "-v" || arg == "--version")
         {
             std::cout << "Version " << version << "\n";
             return 0;
