bb5e99d935064406c62fa4711ac04ce3604eb2a0
Elena Mamontova <helenh463@gmail.com>
2020-02-14 18:36:45 +0200

    Formatting fixed.

--- server.cpp
+++ server.cpp
@@ -83,7 +83,7 @@ int main(int argc, char* argv[])
         boost::asio::io_service io_service;
 
         tcp::resolver resolver(io_service);
-        tcp::acceptor acceptor(io_service, *resolver.resolve(tcp::resolver::query(host,port)));
+        tcp::acceptor acceptor(io_service, *resolver.resolve(tcp::resolver::query(host, port)));
 
         for (;;)
         {
