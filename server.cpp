373691e25f675ff84ef626daad215e6625bbfdd3
Elena Mamontova <helenh463@gmail.com>
2020-03-16 13:31:52 +0200

    The whitespace between '(' and 'err' removed in if(err) construction.

--- server.cpp
+++ server.cpp
@@ -33,7 +33,7 @@ std::string make_daytime_string()
 
 size_t read_complete(char* buff, const error_code& err, size_t bytes)
 {
-    if ( err) return 0;
+    if (err) return 0;
     const std::string str = "\r\n\r\n";
     const bool found = std::search(buff, buff + bytes, str.begin(), str.end()) != buff + bytes;
     return found ? 0 : 1;
