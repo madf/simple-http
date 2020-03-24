3a706e369ebdbca7dd117fe7a8ea7fd054737ba5
Elena Mamontova <helenh463@gmail.com>
2020-03-24 14:19:23 +0200

    The initialization of variables in the constructor Request() changed.

--- request.cpp
+++ request.cpp
@@ -1,10 +1,10 @@
 #include "request.h"
 
 Request::Request(const std::string& start_str)
+    : m_verb(start_str.substr(0, 3)),
+      m_path(start_str.substr(4, start_str.length() - 4 - 9)),
+      m_version(start_str.substr(start_str.length() - 8))
 {
-    m_verb = start_str.substr(0, 3);
-    m_path = start_str.substr(4, start_str.length() - 4 - 9);
-    m_version = start_str.substr(start_str.length() - 8);
 }
 
 const std::string& Request::verb() const
