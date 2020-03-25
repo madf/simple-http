aa0bf7e9f07d98028978313bd5f19a85849f694a
Elena Mamontova <helenh463@gmail.com>
2020-03-25 11:32:08 +0200

    Section private moved below section public.

--- request.h
+++ request.h
@@ -5,16 +5,16 @@
 
 class Request
 {
-    private:
-        std::string m_verb;
-        std::string m_path;
-        std::string m_version;
-
     public:
         Request(const std::string& start_str);
         const std::string& verb() const;
         const std::string& path() const;
         const std::string& version() const;
+
+    private:
+        std::string m_verb;
+        std::string m_path;
+        std::string m_version;
 };
 
 #endif
