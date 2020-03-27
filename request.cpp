b10345371fb7551964bea5126d54e3b2f6159294
Elena Mamontova <helenh463@gmail.com>
2020-03-27 13:49:32 +0200

    The variables size_t pos_space_first and size_t pos_space_second replaced by a constants const size_t pos_space_first and const size_t pos_space second.

--- request.cpp
+++ request.cpp
@@ -2,11 +2,11 @@
 
 Request::Request(const std::string& start_str)
 {
-    size_t pos_space_first = start_str.find_first_of(' ');
+    const size_t pos_space_first = start_str.find_first_of(' ');
 
     m_verb = start_str.substr(0, pos_space_first);
 
-    size_t pos_space_second = start_str.find_last_of(' ');
+    const size_t pos_space_second = start_str.find_last_of(' ');
 
     m_path = start_str.substr(pos_space_first + 1, pos_space_second - pos_space_first - 1);
     m_version = start_str.substr(pos_space_second + 1);
