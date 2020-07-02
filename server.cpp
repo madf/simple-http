ae2de09172076f6b7ac08a7f5be18e439ab603e1
Elena Mamontova <helenh463@gmail.com>
2020-07-02 13:00:58 +0300

    The std::string digits array changed to const std::string digits in the int_to_hex() function.

--- server.cpp
+++ server.cpp
@@ -40,7 +40,7 @@ std::string make_daytime_string()
 
 std::string int_to_hex(int dec)
 {
-    std::string digits[16] = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
+    const std::string digits[16] = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
     std::string hex;
     do
     {
